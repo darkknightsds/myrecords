595c49c958c93d32f7a8eb3fe9b33ea110ca5274
Shane Stafford <shanestafford@live.com>
2017-05-27 18:49:01 -0700

    add MyCollection and MyWishlist classes, add button navigation from Main to MyCollection

--- app/src/main/java/com/epicodus/myrecords/MainActivity.java
+++ app/src/main/java/com/epicodus/myrecords/MainActivity.java
@@ -1,13 +1,32 @@
 package com.epicodus.myrecords;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 
 public class MainActivity extends AppCompatActivity {
+    @Bind(R.id.loginButton) Button mLogInButton;
+//    @Bind(R.id.signupButton) Button mSignUpButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        mLogInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MyCollection.class);
+                startActivity(intent);
+            }
+        });
+
     }
+
 }
