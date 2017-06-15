07cd7f4e42b73cc8d8f4a58232a36fc9d17bc9fa
Shane Stafford <shanestafford@live.com>
2017-06-15 16:02:10 -0700

    add fragments for mycollection and mywishlist to replace activities for saved collection and wishlist

--- app/src/main/java/com/epicodus/myrecords/ui/MyWishlistFragment.java
+++ app/src/main/java/com/epicodus/myrecords/ui/MyWishlistFragment.java
@@ -3,16 +3,29 @@ package com.epicodus.myrecords.ui;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.epicodus.myrecords.Constants;
 import com.epicodus.myrecords.R;
+import com.epicodus.myrecords.adapters.FirebaseWishlistViewHolder;
+import com.epicodus.myrecords.models.Album;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
-/**
- * A simple {@link Fragment} subclass.
- */
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MyWishlistFragment extends Fragment {
+    private DatabaseReference mWishlistRef;
+    private FirebaseRecyclerAdapter mFirebaseAdapter;
+    @BindView(R.id.apiRecycler) RecyclerView mRecyclerView;
 
 
     public MyWishlistFragment() {
@@ -23,8 +36,42 @@ public class MyWishlistFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_wishlist_list, container, false);
+        ButterKnife.bind(this, view);
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        String uid = user.getUid();
+
+        mWishlistRef = FirebaseDatabase
+                .getInstance()
+                .getReference(Constants.FIREBASE_CHILD_WISHLIST)
+                .child(uid);
+
+        setUpFirebaseAdapter();
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my_wishlist, container, false);
+        return view;
+    }
+
+    private void setUpFirebaseAdapter() {
+        mFirebaseAdapter = new FirebaseRecyclerAdapter<Album, FirebaseWishlistViewHolder>
+                (Album.class, R.layout.wishlist_list_cards, FirebaseWishlistViewHolder.class,
+                        mWishlistRef) {
+
+            @Override
+            protected void populateViewHolder(FirebaseWishlistViewHolder viewHolder, Album model, int position) {
+                viewHolder.bindWishlist(model);
+            }
+        };
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        mRecyclerView.setAdapter(mFirebaseAdapter);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        mFirebaseAdapter.cleanup();
     }
 
 }
